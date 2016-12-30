312bbd9b5979be8e32f97e3ed1cf79cd7569a944
mintaz <minhle1508@gmail.com>
2016-12-30 11:24:33 +0700

    Update Function v1

--- CMART5/BUS/BUS_Supplier.cs
+++ CMART5/BUS/BUS_Supplier.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace CMART5.BUS
 {
@@ -11,7 +12,7 @@ namespace CMART5.BUS
         Cmart5DataContext dbt, dbl;
 
 
-        public void AddEditSupplier(bool check, string idSup, DevExpress.XtraEditors.TextEdit txtname, DevExpress.XtraEditors.TextEdit txtsoluong)
+        public void AddEditSupplier(bool check, string idSup, DevExpress.XtraEditors.TextEdit txtname, DevExpress.XtraEditors.TextEdit txtadd, DevExpress.XtraEditors.TextEdit txtphone)
         {
             dbt = new Cmart5DataContext();
             NHACUNGCAP sup;
@@ -23,46 +24,48 @@ namespace CMART5.BUS
                 sup = new NHACUNGCAP();
 
 
-                //try
-                //{
-                //    List<NHACUNGCAP> r = dbt.NHACUNGCAP.ToList().Where(st => st.idNHACUNGCAP.Substring(0, 3) == "LSP").ToList();
-                //    string max = r.Max(t => t.idLOAISANPHAM);
-                //    int idnumber = int.Parse(max.Substring(3, 2)) + 1;
-                //    string maxId = "LSP" + idnumber.ToString().PadLeft(2, '0');
-                //    sup.idLOAISANPHAM = maxId;
-                //}
-                //catch (NullReferenceException)
-                //{
-                //    sup.idLOAISANPHAM = "LSP" + "01";
-                //}
+                try
+                {
+                    List<NHACUNGCAP> r = dbt.NHACUNGCAPs.ToList().Where(st => st.idNHACUNGCAP.Substring(0, 3) == "NCC").ToList();
+                    string max = r.Max(t => t.idNHACUNGCAP);
+                    int idnumber = int.Parse(max.Substring(3, 4)) + 1;
+                    string maxId = "NCC" + idnumber.ToString().PadLeft(4, '0');
+                    sup.idNHACUNGCAP = maxId;
+                }
+                catch (NullReferenceException)
+                {
+                    sup.idNHACUNGCAP = "NCC" + "0001";
+                }
             }
             else
             {
-            //    sup = dbt.LOAISANPHAMs.Where(a => a.idLOAISANPHAM == idSup).SingleOrDefault();
-            //}
-            //sup.TENLOAI = txtname.EditValue.ToString();
-            //sup.SOLUONG = Int32.Parse(txtsoluong.EditValue.ToString());
-            //if (check == true)
-            //{
-            //    dbt.LOAISANPHAMs.InsertOnSubmit(sup);
-            //    dbt.SubmitChanges();
-            //    MessageBox.Show("Thêm loại sản phẩm thành công");
+                sup = dbt.NHACUNGCAPs.Where(a => a.idNHACUNGCAP == idSup).SingleOrDefault();
+            }
+            sup.TENNHACUNGCAP = txtname.EditValue.ToString();
+            sup.DIACHINHACUNGCAP = txtadd.EditValue.ToString();
+            sup.SDT = txtphone.EditValue.ToString();
+            if (check == true)
+            {
+                dbt.NHACUNGCAPs.InsertOnSubmit(sup);
+                dbt.SubmitChanges();
+                MessageBox.Show("Thêm loại sản phẩm thành công");
 
-            //}
-            //else
-            //{
-            //    dbt.SubmitChanges();
-            //    MessageBox.Show("Sửa loại sản phẩm thành công");
+            }
+            else
+            {
+                dbt.SubmitChanges();
+                MessageBox.Show("Sửa loại sản phẩm thành công");
             }
 
 
         }
-        public void loadProductType(string idLSP, DevExpress.XtraEditors.TextEdit txtname, DevExpress.XtraEditors.TextEdit txtsoluong)
+        public void loadSupplier(string idSup, DevExpress.XtraEditors.TextEdit txtname, DevExpress.XtraEditors.TextEdit txtadd, DevExpress.XtraEditors.TextEdit txtphone)
         {
             dbl = new Cmart5DataContext();
-            var loaisp = dbl.LOAISANPHAMs.Where(a => a.idLOAISANPHAM == idLSP).SingleOrDefault();
-            txtname.EditValue = loaisp.TENLOAI;
-            txtsoluong.EditValue = loaisp.SOLUONG;
+            var ncc = dbl.NHACUNGCAPs.Where(a => a.idNHACUNGCAP == idSup).SingleOrDefault();
+            txtname.EditValue = ncc.TENNHACUNGCAP;
+            txtadd.EditValue = ncc.DIACHINHACUNGCAP;
+            txtphone.EditValue = ncc.SDT;
 
         }
     }
